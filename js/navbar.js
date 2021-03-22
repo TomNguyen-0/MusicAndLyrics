a50bbefd7ceb07c6b5ebaaea95df8aa0b933c2e3
TomNguyen-0 <tommie_89@yahoo.com>
2021-03-22 05:39:30 -0700

    moved music into music folder

--- js/navbar.js
+++ js/navbar.js
@@ -11,12 +11,25 @@ function navbar(){
         </div>
     </div>
     `;
-    $(".navbar").append(syntax);
     if(title=="Home"){
+        $(".navbar").append(syntax);
         $("#homePage").addClass("active");
     }else if(title =="Music"){
+        $(".navbar").append(syntax);
         $("#musicPage").addClass("active");
     }else if(title =="Music-Lyrics"){
-    $("#musicPage").addClass("active");
+        var syntax=`
+        <div class="container-fluid">
+            <div class="navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" aria-current="page" href="../index.html" id="homePage">Home</a>
+                    <a class="nav-link" href="../music.html" id="musicPage">Music</a>
+    
+                </div>
+            </div>
+        </div>
+        `;
+        $(".navbar").append(syntax);
+        $("#musicPage").addClass("active");
     }
 }
